5985007fad2bb899cd03b9624bbd68d235b6dc57
Pskmax <maxpskk@gmail.com>
2022-04-20 17:03:26 +0700

    https://github.com/Pskmax/SDP_Project

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../styles/Login.css'
 import bg from '../images/BG.jpg'
+import LoginBootstrap from '../styles/LoginBootstrap'
 
 function Login() {
     return (
         <div className='loginBg' style={{backgroundImage: `url(${bg})`}}>
-            
+            <LoginBootstrap/>
         </div>
     );
 }
