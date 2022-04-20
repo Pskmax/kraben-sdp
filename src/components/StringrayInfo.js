9382be6a9cc19098b065c16ba3279eb8244fe2e0
Pskmax <maxpskk@gmail.com>
2022-04-20 22:46:48 +0700

    master

--- src/components/StringrayInfo.js
+++ src/components/StringrayInfo.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useCart } from 'react-use-cart';
 
-function StringrayInfo({image, name, price}) {
+function StringrayInfo({ image, name, price }, props) {
+  const { addItem } = useCart();
+
   return (
     <div className='krabenInfo'>
-        <div style={{backgroundImage: `url(${image})`}}> </div>
-        <h1> {name} </h1>
-        <p> {price} </p>
-        <Link to="/shop" className='button'>Select</Link>
+      <div style={{ backgroundImage: `url(${image})` }}> </div>
+      <h1> {name} </h1>
+      <p> {price} </p>
+      <div>
+        <button className='button' onClick={() => addItem(props.item)}>Add to Cart</button>
+      </div>
     </div>
   )
 }
 
-export default StringrayInfo
+export default StringrayInfo;
